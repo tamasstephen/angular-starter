bd0d7d1af0416138a9f097758ee30e4b7b1dc8d6
tamasstephen <kicsitnagyon@gmail.com>
2025-01-11 12:03:19 +0100

    chore: add eslint prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { HeaderComponent } from './components/header/header.component';
   selector: 'app-root',
   imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'starter-angular';
